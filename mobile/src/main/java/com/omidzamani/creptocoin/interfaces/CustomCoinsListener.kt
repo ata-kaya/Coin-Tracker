12887d3ce25f42c150ef6cbb77a47296eea500d2
Omid Zamani <zamani.omid@gmail.com>
2018-07-28 16:01:41 +0300

    currency exchange was added

--- mobile/src/main/java/com/omidzamani/creptocoin/interfaces/CustomCoinsListener.kt
+++ mobile/src/main/java/com/omidzamani/creptocoin/interfaces/CustomCoinsListener.kt
@@ -2,7 +2,12 @@ package com.omidzamani.creptocoin.interfaces
 
 /**
  * Created by omidzamani on 22.07.2018.
+ * rhis is custom listener for connecting adapter to the fragment
  */
 interface CustomCoinsListener {
     fun onCoinAddOrRemove()
+}
+
+interface CustomCurrencyListener {
+    fun onCurrencyAddOrRemove()
 }
